4b2adefc2ba5ec220b57f7210d337f4c4a11fe53
Siew Lee Koh <skoh@proofpoint.com>
2024-04-30 17:06:18 +0800

    fixed port issues

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.get('/', function(req, res){
 });
 
 // PORT
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8888;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
