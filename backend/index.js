33ec7099a0c2f6b1e3b6e41adc5cfbaff2cb861c
Siew Lee Koh <skoh@proofpoint.com>
2024-04-30 15:35:14 +0800

    update backend docker files

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.get('/', function(req, res){
 });
 
 // PORT
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
