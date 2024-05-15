c4f1997a280925eb4b36a27469e0d212ba9570e7
Siew Lee Koh <skoh@proofpoint.com>
2024-05-15 23:09:59 +0800

    eks-lb draft

---
+++ terraform/eks-lb.tf
@@ -0,0 +1,73 @@
+################################################################################
+# Load Balancer Role
+################################################################################
+
+module "aws_load_balancer_controller_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.3.1"
+
+  role_name = "aws-load-balancer-controller"
+
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+
+
+################################################################################
+# Aws Load balancer Controller Service Account
+################################################################################
+#
+#resource "kubernetes_service_account" "service-account" {
+#  metadata {
+#    name      = "aws-load-balancer-controller"
+#    namespace = "kube-system"
+#    labels = {
+#      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+#      "app.kubernetes.io/component" = "controller"
+#    }
+#    annotations = {
+#      "eks.amazonaws.com/role-arn"               = module.lb_role.iam_role_arn
+#      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+#    }
+#  }
+#}
+
+################################################################################
+# Install Load Balancer Controler With Helm
+################################################################################
+
+resource "helm_release" "aws_load_balancer_controller" {
+  name = "aws-load-balancer-controller"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.4.4"
+
+  set {
+    name  = "replicaCount"
+    value = 1
+  }
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_id
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.aws_load_balancer_controller_irsa_role.iam_role_arn
+  }
+}
