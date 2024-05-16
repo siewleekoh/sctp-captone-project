211d5020cf086f1834970bd7f7b51b188783ca2d
Siew Lee Koh <skoh@proofpoint.com>
2024-05-16 19:48:23 +0800

    add helm provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,6 +14,12 @@ terraform {
       source  = "hashicorp/cloudinit"
       version = "~> 2.3.4"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+
   }
 
   required_version = "~> 1.3"
@@ -24,4 +30,16 @@ provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.default.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+      command     = "aws"
+    }
+  }
 }
