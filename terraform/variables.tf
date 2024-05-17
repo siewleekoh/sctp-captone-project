4d112f4a3175aa70b8c4284d9d0dae08a289ea54
Siew Lee Koh <skoh@proofpoint.com>
2024-05-17 17:19:21 +0800

    first commit of rds

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,3 +24,23 @@ variable "eks_cluster_name" {
   type        = string
   default     = "ce5-group2-eks-terraform"
 }
+
+variable "user1_name" {
+  type        = string
+  default = "lfchin23"
+}
+
+variable "user2_name" {
+  type        = string
+  default = "jonathan"
+}
+
+variable "mysql_username" {
+  type      = string
+  sensitive = true
+}
+
+variable "mysql_password" {
+  type      = string
+  sensitive = true
+}
