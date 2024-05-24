1b0bec6b545c8fe5066870dd6b89eb3cd6008177
Siew Lee Koh <skoh@proofpoint.com>
2024-05-24 11:18:41 +0800

    exposing backend in dns name in ingress

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -47,72 +47,72 @@ const router = express.Router();
 
 ////////////////////////// FOOD ////////////////////////////////
 // get all Food
-router.get("/api/foods", showFoods);
+router.get("/backend/api/foods", showFoods);
 
 // get single Food 
-router.get("/api/foods/:id", showFoodById);
+router.get("/backend/api/foods/:id", showFoodById);
 
 // create Food
-router.post("/api/foods", createFood);
+router.post("/backend/api/foods", createFood);
 
 // update Food 
-router.put("/api/foods/:id", updateFood);
+router.put("/backend/api/foods/:id", updateFood);
 
 // delete Food
-router.delete("/api/foods/:id", deleteFood);
+router.delete("/backend/api/foods/:id", deleteFood);
 
 
 
 ////////////////////////// USER ////////////////////////////////
 // get all user
-router.get("/api/users/:email", showAUser);
+router.get("/backend/api/users/:email", showAUser);
 
 // create account
-router.post("/api/users/", createAccount);
+router.post("/backend/api/users/", createAccount);
 
 
 
 ////////////////////////// CART ////////////////////////////////
 // add to cart
-router.post("/api/cartItem", addItems);
+router.post("/backend/api/cartItem", addItems);
 
 // get a item in cart
-router.get("/api/cartItem/:user_id/:food_id", getItem);
+router.get("/backend/api/cartItem/:user_id/:food_id", getItem);
 
 // get all items by user id
-router.get("/api/cartItem/:id", allItems);
+router.get("/backend/api/cartItem/:id", allItems);
 
 // update item qty
-router.put("/api/cartItem/", updateItem);
+router.put("/backend/api/cartItem/", updateItem);
 
 // delete a item in cart
-router.delete("/api/cartItem/:user_id/:food_id", deleteItem);
+router.delete("/backend/api/cartItem/:user_id/:food_id", deleteItem);
 
 // delete all items in cart
-router.delete("/api/cartItem/:id", deleteItems);
+router.delete("/backend/api/cartItem/:id", deleteItems);
 
 
 
 ////////////////////////// Booking ////////////////////////////////
-router.post("/api/booking", createBooking);
+router.post("/backend/api/booking", createBooking);
 
 
 
 ////////////////////////// Bill Details ////////////////////////////////
-router.post("/api/billdetails", createBillDetails);
-router.get("/api/billdetails/:id", getBillDetailsById);
+router.post("/backend/api/billdetails", createBillDetails);
+router.get("/backend/api/billdetails/:id", getBillDetailsById);
 
 
 
 ////////////////////////// Bill Status ////////////////////////////////
-router.get("/api/billstatus/new", showNewestStatusId);
-router.post("/api/billstatus", createBillStatus);
-router.get("/api/billstatus/user/:id", getAllBillsByUser);
-router.get("/api/billstatus/bill/:id", getAllBillsByBill);
-router.get("/api/billstatus", getAllBills);
-router.put("/api/billstatus/:id", updateBillStatus);
-router.put("/api/billstatus/paid/:id", updateBillPaid);
-router.put("/api/billstatus/cancel/:id", cancelBillStatus);
+router.get("/backend/api/billstatus/new", showNewestStatusId);
+router.post("/backend/api/billstatus", createBillStatus);
+router.get("/backend/api/billstatus/user/:id", getAllBillsByUser);
+router.get("/backend/api/billstatus/bill/:id", getAllBillsByBill);
+router.get("/backend/api/billstatus", getAllBills);
+router.put("/backend/api/billstatus/:id", updateBillStatus);
+router.put("/backend/api/billstatus/paid/:id", updateBillPaid);
+router.put("/backend/api/billstatus/cancel/:id", cancelBillStatus);
 
 
 
